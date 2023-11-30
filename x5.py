f19996e73097447694f67de6ede552034d355486
Dennis Chiang <dennis8654@gmail.com>
2023-11-30 23:06:58 +0800

    add new file

---
+++ x5.py
@@ -0,0 +1,5 @@
+name = input('please enter your name: ')
+print('Hi', name)
+weight = input('please enter yout weight')
+height = input('please enter your height')
+print('Hi', name, weight, height)
